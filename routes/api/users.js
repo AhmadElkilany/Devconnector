b7e0f163ca954cbc4daedcf84a3f8949304195fe
Kelloo <ahmadelkilany23@gmail.com>
2021-02-14 16:59:38 +0200

    Added Token Creation upon Registering

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
+const config = require("config");
 
 // @route   POST api/users
 // @desc    Register a user
@@ -30,7 +32,9 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
       }
 
       const avatar = gravatar.url(email, {
@@ -50,8 +54,21 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      //Return jsonwebtoken
-      res.send("User Registered");
+      //We return the token to automatically sign in upon register
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
