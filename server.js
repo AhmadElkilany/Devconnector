4efa6174f23d0e91a38014dece900d49219d7d38
Kelloo <ahmadelkilany23@gmail.com>
2021-02-14 13:57:15 +0200

    Added User Registration

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-//Connect to Database
+// Connect to Database
 connectDB();
 
-app.get('/', (req,res) => res.send('API Running'));
+// Init Middleware
+// This allows us to retrieve the req.body
+app.use(express.json({extended: false}));
+
+app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
