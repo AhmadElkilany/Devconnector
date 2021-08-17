36d02a19deac3995ff3808492283233642fb402c
Kelloo <ahmadelkilany23@gmail.com>
2021-08-17 18:45:04 +0200

    fixed minor issue

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 // Redux
-import { Provider, provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
 
 const App = () => (
