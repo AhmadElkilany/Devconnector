775bd2034abfc5419d6329018e24d4a85439b064
Kelloo <ahmadelkilany23@gmail.com>
2021-08-22 13:39:08 +0200

    Added Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
